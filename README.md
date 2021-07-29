70902a21a9ab3cc8f4485ceb6ffa195e55b8144b
Mark Irvine <mark@wbb.ai>
2021-07-29 16:23:11 +0100

    Added old website

---
+++ README.md
@@ -0,0 +1,4 @@
+# My Personal Website
+
+This repo contains the source code for my personal website, viewable at https://website-mark-s-irvine.firebaseapp.com/. 
+This website shows of some of the projects I have worked on and detail the experience I have.
---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBboQBviClwkJLH-t5t-d8j3BHCj-ojxG8",
+    authDomain: "website-mark-s-irvine.firebaseapp.com",
+    databaseURL: "https://website-mark-s-irvine.firebaseio.com",
+    projectId: "website-mark-s-irvine",
+    storageBucket: "",
+    messagingSenderId: "363642595998"
+  };
+  firebase.initializeApp(config);
